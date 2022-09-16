fed89d0691973ae76c9987a20294ecb7a72b9012
Kürşat Şimşek <kursatsimsek@Kursats-MacBook-Pro.local>
2022-09-16 17:13:39 +0300

    docker file written for node and react projects

---
+++ node-docker/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/team', (req, res) => {
+    res.send('GALATASARAY')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
